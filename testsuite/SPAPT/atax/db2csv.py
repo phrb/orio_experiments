918880cc3384317e240876f7fbd6844a501fff16
Pedro Bruel <pedro.bruel@gmail.com>
2018-07-18 15:54:00 +0200

    Update results

--- testsuite/SPAPT/atax/db2csv.py
+++ testsuite/SPAPT/atax/db2csv.py
@@ -2,6 +2,16 @@
 
 import dataset, csv
 
+database = dataset.connect("sqlite:///search_space.db")
+
+experiments = database["experiments"]
+
+with open("search_space.csv", "w") as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames = experiments.find_one(id = 1).keys())
+
+    writer.writeheader()
+    writer.writerows(experiments)
+
 database = dataset.connect("sqlite:///results.db")
 
 experiments = database["results"]
