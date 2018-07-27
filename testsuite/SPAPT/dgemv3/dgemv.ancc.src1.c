063076e35cf66cd07fffc88b04bf1952f0094f32
Pedro Bruel <pedro.bruel@gmail.com>
2018-07-27 14:28:02 +0200

    Add initial model for dgemv3

--- testsuite/SPAPT/dgemv3/dgemv.ancc.src1.c
+++ testsuite/SPAPT/dgemv3/dgemv.ancc.src1.c
@@ -1,20 +1,20 @@
 /* void DGEMV(int A_nrows, int A_ncols, double A[A_ncols][A_nrows], int e_nrows, double e[e_nrows], int w_nrows, double w[w_nrows], int x_nrows, double x[x_nrows], int p_nrows, double p[p_nrows], int y_nrows, double y[y_nrows], int z_nrows, double z[z_nrows]){
-	int ii,i,j;
-	double t2[A_nrows];
+    int ii,i,j;
+    double t2[A_nrows];
 */
 
-/*@ begin PerfTuning (        
+/*@ begin PerfTuning (
   def build
   {
     arg build_command = 'gcc -O3 -fopenmp -DDYNAMIC';
     arg libs = '-lm -lrt';
   }
-   
-  def performance_counter         
+
+  def performance_counter
   {
     arg repetitions = 35;
   }
-  
+
   def performance_params
   {
     # Cache tiling
@@ -22,27 +22,27 @@
     param T1_J[] = [1,16,32,64,128,256,512];
     param T1_Ia[] = [1,64,128,256,512,1024,2048];
     param T1_Ja[] = [1,64,128,256,512,1024,2048];
-    
+
     param T3_I[] = [1,16,32,64,128,256,512];
     param T3_J[] = [1,16,32,64,128,256,512];
     param T3_Ia[] = [1,64,128,256,512,1024,2048];
     param T3_Ja[] = [1,64,128,256,512,1024,2048];
 
-    
+
     param T5_I[] = [1,16,32,64,128,256,512];
     param T5_J[] = [1,16,32,64,128,256,512];
     param T5_Ia[] = [1,64,128,256,512,1024,2048];
     param T5_Ja[] = [1,64,128,256,512,1024,2048];
 
-    
+
     param T7_I[] = [1,16,32,64,128,256,512];
     param T7_J[] = [1,16,32,64,128,256,512];
     param T7_Ia[] = [1,64,128,256,512,1024,2048];
     param T7_Ja[] = [1,64,128,256,512,1024,2048];
 
 
-    # Unroll-jam 
-    param U1_I[]  = [1,4,8,12,16]; 
+    # Unroll-jam
+    param U1_I[]  = [1,4,8,12,16];
     param U1_J[]  = [1,4,8,12,16];
     param U2_I[]  = [1,4,8,12,16];
     param U3_I[]  = [1,4,8,12,16];
@@ -66,7 +66,7 @@
     param RT5_J[] = [1,8,32];
     param RT7_I[] = [1,8,32];
     param RT7_J[] = [1,8,32];
-    
+
 
     # Scalar replacement
     param SCR[]  = [False,True];
@@ -96,31 +96,37 @@
     constraint tileI7 = ((T7_Ia == 1) or (T7_Ia % T7_I == 0));
     constraint tileJ7 = ((T7_Ja == 1) or (T7_Ja % T7_J == 0));
 
-    
+
     constraint reg_capacity_1 = (RT1_I*RT1_J <= 150);
     constraint reg_capacity_3 = (RT3_I*RT3_J <= 150);
     constraint reg_capacity_5 = (RT5_I*RT5_J <= 150);
     constraint reg_capacity_7 = (RT7_I*RT7_J <= 150);
-    
+
     constraint unroll_limit_1 = (U1_I == 1) or (U1_J == 1);
     constraint unroll_limit_3 = (U3_I == 1) or (U3_J == 1);
     constraint unroll_limit_5 = (U5_I == 1) or (U5_J == 1);
     constraint unroll_limit_7 = (U7_I == 1) or (U7_J == 1);
 
-  }  
-  
+  }
+
   def search
   {
-    arg algorithm = 'Randomsearch';
-    arg total_runs = 250;
+    arg algorithm = 'DLMT';
+    arg total_runs = 100;
+    arg dlmt_federov_sampling = 20;
+    arg dlmt_extra_experiments = 20;
+    arg dlmt_steps = 4;
+    arg dlmt_linear = '["SCR", "VEC1", "VEC2", "VEC3", "VEC4", "VEC5", "VEC6", "VEC7", "VEC8", "VEC9", "VEC10"]';
+    arg dlmt_quadratic = '[ "T1_I", "T1_J", "T1_Ia", "T1_Ja", "T3_I", "T3_J", "T3_Ia", "T3_Ja", "T5_I", "T5_J", "T5_Ia", "T5_Ja", "T7_I", "T7_J", "T7_Ia", "T7_Ja", "U1_I", "U1_J", "U2_I", "U3_I", "U3_J", "U4_I", "U5_I", "U5_J", "U6_I", "U7_I", "U7_J", "U8_I", "U9_I", "U10_I", "RT1_I", "RT1_J", "RT3_I", "RT3_J", "RT5_I", "RT5_J", "RT7_I", "RT7_J"]';
+    #arg dlmt_interactions = '["T1_I:T1_J", "T1_J:T1_K", "T1_I:T1_K", "T2_I:T2_J", "T2_J:T2_K", "T2_I:T2_K"]';
   }
-  
+
   def input_params
   {
     param M = 5000;
     param N = 5000;
-  } 
- 
+  }
+
   def input_vars {
     decl int A_nrows = M;
     decl int A_ncols = N;
@@ -134,7 +140,7 @@
     decl dynamic double t2[M] = 0;
     decl dynamic double t6[M] = 0;
     decl dynamic double t10[M] = 0;
-  }            
+  }
   def validation {
 
     arg validation_file = 'validation.c';
@@ -152,7 +158,7 @@ int iii, jjj, kkk;
 
 
 /*@ begin Loop (
-  
+
   transform Composite(
       tile = [('i',T1_I,'ii'),('j',T1_J,'jj'),
              (('ii','i'),T1_Ia,'iii'),(('jj','j'),T1_Ja,'jjj')],
@@ -161,17 +167,17 @@ int iii, jjj, kkk;
       regtile = (['i','j'],[RT1_I,RT1_J]),
       vector = (VEC1, ['ivdep','vector always'])
    )
-    for (i = 0; i <= A_ncols-1; i++) 
-        for (j = 0; j <= A_nrows-1; j++) 
-            t2[j] = t2[j] + A[i][j]*x[i]; 
+    for (i = 0; i <= A_ncols-1; i++)
+        for (j = 0; j <= A_nrows-1; j++)
+            t2[j] = t2[j] + A[i][j]*x[i];
 
 
   transform Composite(
       unrolljam = (['i'],[U2_I]),
       vector = (VEC2, ['ivdep','vector always'])
    )
-   for (i = 0; i <= A_nrows-1; i++) 
-        y[i] = t2[i]+y[i]; 
+   for (i = 0; i <= A_nrows-1; i++)
+        y[i] = t2[i]+y[i];
 
    transform Composite(
       tile = [('i',T3_I,'ii'),('j',T3_J,'jj'),
@@ -181,19 +187,19 @@ int iii, jjj, kkk;
       regtile = (['i','j'],[RT3_I,RT3_J]),
       vector = (VEC3, ['ivdep','vector always'])
    )
-    for (i = 0; i <= A_ncols-1; i++) 
-       for (j = 0; j <= A_nrows-1; j++) 
-            t6[j] = t6[j] + A[i][j]*w[i]; 
+    for (i = 0; i <= A_ncols-1; i++)
+       for (j = 0; j <= A_nrows-1; j++)
+            t6[j] = t6[j] + A[i][j]*w[i];
 
   transform Composite(
       unrolljam = (['i'],[U4_I]),
       vector = (VEC4, ['ivdep','vector always'])
    )
-   for (i = 0; i <= A_nrows-1; i++) 
-        z[i] = t6[i]+z[i]; 
+   for (i = 0; i <= A_nrows-1; i++)
+        z[i] = t6[i]+z[i];
 
 
-    	
+
     transform Composite(
       tile = [('i',T5_I,'ii'),('j',T5_J,'jj'),
              (('ii','i'),T5_Ia,'iii'),(('jj','j'),T5_Ja,'jjj')],
@@ -202,16 +208,16 @@ int iii, jjj, kkk;
       regtile = (['i','j'],[RT5_I,RT5_J]),
       vector = (VEC5, ['ivdep','vector always'])
    )
-    for (i = 0; i <= A_ncols-1; i++) 
-        for (j = 0; j <= A_nrows; j++) 
-            t10[j] = t10[j] + A[i][j]*e[i]; 
+    for (i = 0; i <= A_ncols-1; i++)
+        for (j = 0; j <= A_nrows; j++)
+            t10[j] = t10[j] + A[i][j]*e[i];
 
   transform Composite(
       unrolljam = (['i'],[U6_I]),
       vector = (VEC6, ['ivdep','vector always'])
    )
-    for (i = 0; i <= A_nrows-1; i++) 
-        p[i] = t10[i]+p[i]; 
+    for (i = 0; i <= A_nrows-1; i++)
+        p[i] = t10[i]+p[i];
 
 
      transform Composite(
@@ -222,35 +228,35 @@ int iii, jjj, kkk;
       regtile = (['i','j'],[RT7_I,RT7_J]),
       vector = (VEC7, ['ivdep','vector always'])
    )
-    for (i = 0; i <= A_ncols-1; i++) 
+    for (i = 0; i <= A_ncols-1; i++)
        for (j = 0; j <= A_nrows-1; j++) {
-			t10[j] = t10[j] + e[i]*A[i][j]; 
-			t6[j] = t6[j] + w[i]*A[i][j]; 
-			t2[j] = t2[j] + A[i][j]*x[i]; 
-		}
+            t10[j] = t10[j] + e[i]*A[i][j];
+            t6[j] = t6[j] + w[i]*A[i][j];
+            t2[j] = t2[j] + A[i][j]*x[i];
+        }
 
 
   transform Composite(
       unrolljam = (['i'],[U8_I]),
       vector = (VEC8, ['ivdep','vector always'])
    )
-     for (i = 0; i <= A_nrows-1; i++) 
-       y[i] = t2[i]+y[i]; 
+     for (i = 0; i <= A_nrows-1; i++)
+       y[i] = t2[i]+y[i];
 
   transform Composite(
       unrolljam = (['i'],[U9_I]),
       vector = (VEC9, ['ivdep','vector always'])
    )
-     for (i = 0; i <= A_nrows-1; i++) 
-       z[i] = t6[i]+z[i]; 
+     for (i = 0; i <= A_nrows-1; i++)
+       z[i] = t6[i]+z[i];
 
 
   transform Composite(
       unrolljam = (['i'],[U10_I]),
       vector = (VEC10, ['ivdep','vector always'])
    )
-     for (i = 0; i <= A_nrows-1; i++) 
-       p[i] = t10[i]+p[i]; 
+     for (i = 0; i <= A_nrows-1; i++)
+       p[i] = t10[i]+p[i];
 
 ) @*/
 
