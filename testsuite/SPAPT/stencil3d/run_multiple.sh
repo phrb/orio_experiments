4e2ee71b40cb53f32bea40ffa2e6c5953764f71b
Pedro Bruel <pedro.bruel@gmail.com>
2018-08-31 22:34:38 +0200

    Improve launch script

--- testsuite/SPAPT/stencil3d/run_multiple.sh
+++ testsuite/SPAPT/stencil3d/run_multiple.sh
@@ -1,5 +1,17 @@
-#!/bin/bash
+#! /bin/bash
+
 for i in `seq 1 $1`;
 do
 	../../../orcc -v -e $2
-done    
+done
+
+mkdir xeon_e5_2630_v3_$(uname -n | cut -d. -f1)
+
+./db2csv.py
+mv results.* search_space* xeon_e5_2630_v3_$(uname -n | cut -d. -f1)
+
+./clean.sh
+
+git add --all
+git commit -m "stencil3d Xeon E5 2630 v3 $(uname -n | cut -d. -f1) $(date)"
+git push
